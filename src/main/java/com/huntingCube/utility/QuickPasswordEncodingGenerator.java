e9a827cecbcdbe961b99601e9abd40f0b19e3320
dravit <dravit.info@gmail.com>
2017-05-22 16:26:07 +0530

    Add files via upload

--- src/main/java/com/huntingCube/utility/QuickPasswordEncodingGenerator.java
+++ src/main/java/com/huntingCube/utility/QuickPasswordEncodingGenerator.java
@@ -8,7 +8,7 @@ public class QuickPasswordEncodingGenerator {
      * @param args
      */
     public static void main(String[] args) {
-            String password = "abc123";
+            String password = "dravit";
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             System.out.println(passwordEncoder.encode(password));
     }
